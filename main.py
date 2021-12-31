3519f80597b3c5e7c92cf89a3ae46a8b562f2ca8
Sania <sania.d@myplanet.com>
2021-12-30 20:43:22 -0500

    Working program without exe installer

--- main.py
+++ main.py
@@ -3,49 +3,43 @@ import csv
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-# depending on the input from the UI , choose the topic to create results for
-# import dicts  # where dicts is a file with dictionaries of data
-# if input =="work":
-#     d = dicts.work
-# else input=="something else":
-#     d = dicts.something
 
-# URL to scrape
-url = "https://realpython.github.io/fake-jobs/jobs/senior-python-developer-0.html"
+def main():
 
-# get the page and scrape
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
+    # URL to scrape
+    url = "https://realpython.github.io/fake-jobs/jobs/senior-python-developer-0.html"
 
-result = soup.find_all(id="ResultsContainer")
-titles = []
-descs = []
-dates = []
-for x in result:
-    title = x.find("h1")
-    desc = x.find('p')
-    date = x.find("p", {"id" : "date"})
-    titles.append(title.text)
-    descs.append(desc.text)
-    date = datetime.strptime(date.text.split(' ')[1], '%Y-%m-%d').date()
-    dates.append(date)
+    # get the page and scrape
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
 
+    result = soup.find_all(id="ResultsContainer")
 
-# get number of results to decide columns
-header = ["title", "description", "date"]
+    titles = []
+    descs = []
+    dates = []
+    for x in result:
+        title = x.find("h1")
+        desc = x.find('p')
+        date = x.find("p", {"id" : "date"})
+        titles.append(title.text)
+        descs.append(desc.text)
+        date = datetime.strptime(date.text.split(' ')[1], '%Y-%m-%d').date()
+        dates.append(date)
 
-# push results to csv (method)
-with open('test_results.csv', 'w', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    # write the header
-    writer.writerow(header)
-    # get data to write
-    for r in zip(titles, descs, dates):
-        row = list(r)
-        # write the data
-        writer.writerow(row)
+    # get number of results to decide columns
+    header = ["title", "description", "date"]
 
+    # push results to csv (method)
+    filename = "scrap_" + str(date.today()) + ".csv"
+    with open(filename, 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
+        # get data to write
+        for r in zip(titles, descs, dates):
+            row = list(r)
+            # write the data
+            writer.writerow(row)
 
-# some processing for example sorting desc by date of posting
-
-# make this file importable and usable in diff file
+    return 0
--- ui.py
+++ ui.py
@@ -1,31 +1,21 @@
 from tkinter import *
 from tkinter import ttk
-import main
+from main import *
 
 window = Tk()
 window.title('Scrappit')
-window.geometry("400x80")
-window.configure(bg='pink')
+window.geometry("190x50")
+window.configure(bg='green')
 
-mainframe = ttk.Frame(window, padding="20 20 20 20")
+mainframe = ttk.Frame(window)
 
-ttk.Label(window, text="Topic could be any of \"job\", etc").grid(column=2, row=0)
-ttk.Label(window, text="Enter the topic ").grid(column=1, row=1)
-
-topic = StringVar()
-topic_entry = ttk.Entry(window, width=20, textvariable=topic)
-topic_entry.grid(column=2, row=1)
-
-ttk.Button(window, text="Get CSV").grid(column=1, row=3)
-# ttk.Button(window, text="Get CSV", command=main).grid(column=1, row=3)
+ttk.Button(window, text="Get CSV", command=main).grid(column=2, row=1)
+ttk.Button(window, text="Quit", command=window.destroy).grid(column=4, row=1)
 
 # ttk.Label(window, text="The CSV results are downloaded").grid(column=1, row=4)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
-topic_entry.focus()
-window.bind("<Return>")
-
-
+window.quit()
 window.mainloop()
